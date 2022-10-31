2e65eef787c8405969605be992ca296e4bda8347
aimajoke <aimajoke16@gmail.com>
2022-10-30 23:30:36 -0500

    terminada funcionalidad searchPage

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ const api = axios.create({
     }
 })
 
-
-
 backButton.addEventListener('click', goBack)
 searchButton.addEventListener('click', searchMovie)
+searchInput.addEventListener('keydown', (event)=>{
+    if(event.key == 'Enter'){
+        searchMovie()
+    }
+})
 
 function redondear(numero){
     if(numero == 0){
@@ -19,15 +22,9 @@ function redondear(numero){
     }
 }
 
-async function getTrendings(){
-    const {data} = await api('/trending/movie/day') 
-    console.log(data.results)
-    const randomMovies = []
-    const heroMovie = data.results[data.results.length-2]
-    heroMovieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w500'+heroMovie.poster_path)
-    heroMovieTitle.textContent = heroMovie.title
-    heroMovieScore.textContent = redondear(heroMovie.vote_average)
-    data.results.forEach(movie => {
+function render(movieList){
+    const moviesRendered = []
+    movieList.forEach(movie => {
         const movieContainer = document.createElement('article');
         const imgRandomMovieContainer = document.createElement('div');
         const movieImg = document.createElement('img');
@@ -49,31 +46,138 @@ async function getTrendings(){
         movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w342' + movie.poster_path)
         tituloRandomMovie.textContent = movie.title
         imbdSpan.textContent = redondear(movie.vote_average)
-
-        randomMovies.push(movieContainer)
-    });
-    trendingMoviesContainer.append(...randomMovies)
+        moviesRendered.push(movieContainer)
+    })
+    return moviesRendered
 }
 
-async function getCategories(){
+async function getTrendings(){
+    const {data} = await api('/trending/movie/day') 
+    console.log(data.results)
+
+    const heroMovie = data.results[data.results.length-2]
+    heroMovieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w500'+heroMovie.poster_path)
+    heroMovieTitle.textContent = heroMovie.title
+    heroMovieScore.textContent = redondear(heroMovie.vote_average)
+    trendingMoviesContainer.innerHTML = ''
+
+    const movieList = render(data.results)
+    trendingMoviesContainer.append(...movieList)
+}
+
+async function categoryList(){
     const {data} = await api('/genre/movie/list') 
+    const myList = {}
+    data.genres.forEach(genre=>{
+        myList[genre.id] = genre.name
+    })
+    return myList
+}
+
+async function getCategoryList(){
+    const categories = await categoryList()
     const buttons = []
-    data.genres.forEach(genero => {
+    console.log(categories)
+    for(genre in categories){
+        const name =  categories[genre]
+        const id =  genre
         const categoryButton = document.createElement('button')
         categoryButton.setAttribute('type', 'button')
-        categoryButton.setAttribute('id', `genre-${genero.id}`)
+        categoryButton.setAttribute('id', `genre-${id}`)
         categoryButton.classList.add('categoryButton')
-        categoryButton.textContent = genero.name
+        categoryButton.textContent = name
+        categoryButton.addEventListener('click', ()=>location.hash=`#category=${name}-${id}`)
         buttons.push(categoryButton)
-    })
+    }
+    categorySection.innerHTML = ''
     categorySection.append(...buttons)
 }
 
+async function getCategory(id){
+    const {data} = await api('/discover/movie',{
+        params: {
+            with_genres: id
+        }
+    }) 
+    moviesByCategory.innerHTML = ''
+    const movieList = render(data.results)
+    moviesByCategory.append(...movieList)
+}
+
+function render2(movieList, categories){
+    const movies = []
+    const moviesFaceless = []
+    movieList.forEach(movie=>{
+        const article = document.createElement('article')
+        const imgContainer = document.createElement('div')
+        const img = document.createElement('img')
+        const description = document.createElement('div')
+        const titulo = document.createElement('h4')
+        const genres = document.createElement('span')
+        const features = document.createElement('div')
+        const imbdButton = document.createElement('span')
+        const date = document.createElement('span')
+        const age = document.createElement('span')
+        const language = document.createElement('span')
+
+        article.classList.add('searchResultArticle')
+        imgContainer.classList.add('imgResultContainer')
+        img.classList.add('imgResult')
+        genres.classList.add('searchResultSubtitle')
+        description.classList.add('descriptionResultContainer')
+        titulo.classList.add('tituloRandomMovie')
+        titulo.classList.add('text-left')
+        features.classList.add('featuresOfResult')
+        imbdButton.classList.add('spanSquare')
+        imbdButton.classList.add('imbdSpan')
+        imbdButton.classList.add('mt-1')
+        date.classList.add('spanSquare')
+        language.classList.add('spanSquare')
+        age.classList.add('spanSquare')
+
+        img.setAttribute('src', 'https://image.tmdb.org/t/p/w342'+ movie.poster_path)
+        titulo.textContent = movie.title
+        genres.textContent = categories[movie.genre_ids[0]]
+        imbdButton.textContent = redondear(movie.vote_average)
+        const [year, month, day] = movie.release_date.split('-')
+        date.textContent = year
+        age.textContent = movie.adult? '+18' : '+13'
+        language.textContent = movie.original_language
+        features.append(imbdButton, age, date, language)
+        description.append(titulo, genres, features)
+        imgContainer.append(img)
+        article.append(imgContainer, description)
+        if(movie.poster_path){
+            movies.push(article)
+        }else{
+            moviesFaceless.push(article)
+        }
+    })
+    return [...movies, ...moviesFaceless]
+}
+
+async function searchQuery(query){
+    searchInputText.textContent =`for "${query}"` 
+    const {data} = await api('/search/movie',{
+        params: {
+            query,
+        }
+    })
+    console.log(data.results)
+    const categories = await categoryList()
+    const movies = render2(data.results, categories)
+    searchResultsSection.innerHTML = ''
+    searchResultsSection.append(...movies)
+}
+
 function searchMovie(){
-    location.hash = '#search='
+    if(searchInput.value.length < 3){
+        console.log('tu madre')
+    }else{
+        location.hash = '#search=' + (searchInput.value.trim().split(' ')).join('&')
+    }
 }
 
 function goBack(){
-    location.hash = '#home'
-
+    location.hash = window.history.back()
 }
--- src/navigator.js
+++ src/navigator.js
@@ -9,6 +9,7 @@ function removeHidden(elementos){
 }
 
 function navigator(){
+    window.scrollTo(0,0)
     if(location.hash.startsWith('#trends')){
         trendsPage()
     }
@@ -16,10 +17,12 @@ function navigator(){
         moviePage()
     }
     else if(location.hash.startsWith('#search=')){
-        searchPage()
+        const [_, query] = location.hash.split('=')
+        searchPage(query)
     }
     else if(location.hash.startsWith('#category=')){
-        categoryPage()
+        const [_, category] = location.hash.split('=')
+        categoryPage(category)
     }
     else{
         homePage()
@@ -27,49 +30,49 @@ function navigator(){
 
 }
 
-function searchPage(){
+function searchPage(query){
     const toRemove = [backButton, tuneButton,searchPageTitle, searchInputContainer, searchResultsSection]
-    const toAdd = [movieDescription, heroPoster, pageLogo, trendingSection, shortCategories, categorySection, verMasTrends]
+    const toAdd = [movieDescription, heroPoster, pageLogo, trendingSection, categorySection, verMasTrends]
     addHidden(toAdd)
     removeHidden(toRemove)
     searchInputContainer.classList.add('mt-28')
     trendingSectionTitle.textContent = 'Trending Movies'
+    searchQuery(query)
 }
 function trendsPage(){
     const toRemove = [backButton, tuneButton, trendingSectionTitle, ]
-    const toAdd = [movieDescription, heroPoster, pageLogo, shortCategories, categorySection, searchResultsSection, searchInputContainer, searchInputContainer, searchPageTitle, verMasTrends]
+    const toAdd = [movieDescription, heroPoster, pageLogo, categorySection, searchResultsSection, searchInputContainer, searchInputContainer, searchPageTitle, verMasTrends]
     trendingSection.classList.add('pt-20')
     addHidden(toAdd)
     removeHidden(toRemove)
-
+    
     trendingMoviesContainer.classList.add('flex-wrap')
     trendingMoviesContainer.classList.add('justify-evenly')
     getTrendings()
 }
-function categoryPage(){
-    const toRemove = [backButton, tuneButton, trendingSectionTitle, ]
-    const toAdd = [movieDescription, heroPoster, pageLogo, shortCategories, categorySection, searchResultsSection, searchInputContainer, searchInputContainer, searchPageTitle, verMasTrends]
-    trendingSection.classList.add('pt-20')
-    addHidden(toAdd)
-    removeHidden(toRemove)
-    trendingSectionTitle.textContent = 'Action'
-    trendingMoviesContainer.classList.add('flex-wrap')
-    trendingMoviesContainer.classList.add('justify-evenly')
-    getTrendings()
-}
-
 function homePage(){
-    const toAdd = [backButton,tuneButton,movieDescription,searchPageTitle,searchResultsSection,]
-    const toRemove = [searchInputContainer,categorySection,pageLogo,heroPoster,trendingSection,categorySection,shortCategories, verMasTrends]
+    const toAdd = [backButton,tuneButton,movieDescription,searchPageTitle,searchResultsSection, moviesByCategory]
+    const toRemove = [searchInputContainer,categorySection,pageLogo,heroPoster,trendingSection,categorySection, verMasTrends, trendingMoviesContainer]
     addHidden(toAdd)
     removeHidden(toRemove)
-    trendingSection.classList.remove('pt-20')
+    
     trendingMoviesContainer.classList.remove('flex-wrap')
     trendingMoviesContainer.classList.remove('justify-evenly')
+    trendingSection.classList.remove('pt-20')
     trendingSectionTitle.textContent = 'Trending Movies'
     searchInputContainer.classList.remove('mt-28')
     getTrendings()
-    getCategories()
+    getCategoryList()
+}
+function categoryPage(category){
+    const [categoryName, categoryId] = category.split('-')
+    const toRemove = [backButton, tuneButton, trendingSectionTitle, moviesByCategory]
+    const toAdd = [movieDescription, heroPoster, pageLogo, categorySection, searchResultsSection, searchInputContainer, searchInputContainer, searchPageTitle, verMasTrends, trendingMoviesContainer]
+    addHidden(toAdd)
+    removeHidden(toRemove)
+    trendingSectionTitle.textContent = categoryName
+    getCategory(categoryId)
+    trendingSectionTitle.classList.add('pt-20')
 }
 
 function moviePage(){
